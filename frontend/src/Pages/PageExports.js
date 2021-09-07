8b9bc1a5f35fcd2624dee95bdeed8dfbee53541f
Jack Beard <jacko.2233@hotmail.com>
2021-09-08 02:59:25 +1000

    Update PageExports.js

--- frontend/src/Pages/PageExports.js
+++ frontend/src/Pages/PageExports.js
@@ -2,5 +2,5 @@ export { default as Landing } from './Landing';
 export { default as Login } from './Login';
 export { default as Register } from './Register';
 export { default as ConfirmEmail } from './ConfirmEmail';
-export { default as SendResetEmail } from './ResetPassword/sendResetEmail';
-export { default as PasswordChange } from './ResetPassword/passwordChange';
+export { default as SendResetEmail } from './ResetPassword/SendResetEmail';
+export { default as PasswordChange } from './ResetPassword/PasswordChange';
