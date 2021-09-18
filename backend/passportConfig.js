046ac337c9bdba2dd9c4cb3cda65d1a68ddc10e4
Jack Beard <jacko.2233@hotmail.com>
2021-09-18 14:04:13 +1000

    small change to code spacing

--- backend/passportConfig.js
+++ backend/passportConfig.js
@@ -5,33 +5,24 @@ const localStrategy = require('passport-local').Strategy;
 module.exports = (passport) => {
     
     passport.use(
-
         // set up a local strategy for authenticating a login request
         new localStrategy({usernameField: 'email', passwordField: 'password'}, (email, password, done) => {
-        
             // look for an email in our DB matching the form email input
             User.findOne({ email: email }, (err, user) => {
-
                 if (err) throw err;
-                
                 // if no matching email, return no error, no user
                 if (!user) return done(null, false);
-
                 // if user exists, compare the given form password to the encrypted DB password
                 bcrypt.compare(password, user.password, (err, result) => {
-                    
                     if (err) throw err;
-
                     // if passwords match, return no error, and the user
                     if (result === true) {
                         return done(null, user);
                     } 
-                    
                     // if passwords don't match, return no error, no user
                     else {
                         return done(null, false);
                     }
-
                 });
             });
         })
@@ -39,20 +30,16 @@ module.exports = (passport) => {
 
     // takes a user and gives them a cookie, which has its own ID
     passport.serializeUser((user, done) => {
-
         done(null, user.id);
-
     });
 
     // takes a cookie, unwraps it, and returns a user (if any), assosciated with its ID.
     passport.deserializeUser((id, done) => {
-        
+    
         // look for ids (primary key referenced by _id) in DB that match ID of our cookie we are unwrapping.
         User.findOne({ _id: id }, (err, user) => {
-        
             // if we get an error trying to find a user associated with the cookie
             if (err) throw err;
-        
             // if no errors
             // this is what will be returned by req.user on incoming requests - can always change to include more/less info
             const userInformation = {
@@ -61,10 +48,10 @@ module.exports = (passport) => {
                 email: user.email,
                 confirmedEmail: user.confirmedEmail
             };
-
             // return error status + userInformation
             done(err, userInformation);
-
         });
+
     });
+
 };
--- backend/server.js
+++ backend/server.js
@@ -47,24 +47,19 @@ app.post('/login', (req, res, next) => {
 
     // authenticate the user using our local passport strategy
     passport.authenticate('local', (err, user, info) => {
-
         if (err) {
             res.send(err);
         };
-
         // if authentication failed
         if (!user) {
             res.status(400).send('Your email or password is incorrect.')
         }
-
         // if authentication was successful
         else {
             req.login(user, (err) => {
-
                 if (err) {
                     throw err
                 }
-
                 // if no errors, log in is successful, send user details to client
                 console.log('You have been authenticated! Hello there!');
                 res.status(200).send({'id': req.user.id, 'firstName': req.user.firstName, 'lastName': req.user.lastName, 'email': req.user.email, 'confirmedEmail': req.user.confirmedEmail});
@@ -79,23 +74,19 @@ app.post('/register', (req, res) => {
 
     // check if a user with the given email already exists
     User.findOne({email: req.body.email}, async (err, doc) => {
-
         if (err) {
             res.status(400).send(err);
         };
-
         // if user already exists, don't register
         if (doc) {
             res.status(400).send('A user already exists with that email!');
         }
-
         // if user doesn't exist, register a new user
         if (!doc) {
             const pword = req.body.password;
             const pwordConfirm = req.body.confirmPassword;
             // encrypt the password the user gives, so it is not stored as plaintext in the DB
             const hashedPword = await bcrypt.hash(req.body.password, 10);
-
             // if passwords in the register form match and there is no existing email in the DB, create a new user
             if (pword === pwordConfirm) {
                 const newUser = new User({
@@ -105,16 +96,12 @@ app.post('/register', (req, res) => {
                     password: hashedPword,
                     confirmedEmail: false
                 });
-
                 // get details (including uuid) of this newly registered user
                 const savedNewUser = await newUser.save();
-                
                 // send confirmation email with email address and uuid attached
                 sendConfirmationEmail(req.body.email, savedNewUser._id);
-
                 res.status(200).send('Registered into DB successfully!');
             }
-            
             else {
                 res.status(400).send('Passwords mismatch! Could not register.')
             }
@@ -131,20 +118,14 @@ app.post('/confirmEmail', (req, res) => {
 
     const userID = req.body.userid;
     let confirmedEmail = null;
-
     console.log('got incoming id:', userID);
-
     // if email hasn't been confirmed
     User.findById(userID, (err, result) => {
-
         if (err) {
             res.status(400).send(err);
         }
-
         else {
-            
             confirmedEmail = result.confirmedEmail;
-            
             // only update if email isn't already confirmed
             if (confirmedEmail === false) {
                 User.findByIdAndUpdate(
@@ -162,16 +143,14 @@ app.post('/confirmEmail', (req, res) => {
                     }
                 );
             }
-        
             // if it has been confirmed already
             else {
                 res.status(400).send('Email already confirmed!');
             }
         }
     });
-
     console.log('confirmed email:', confirmedEmail)
-    
+
 })
 
 // accepts an email
@@ -181,28 +160,20 @@ app.post('/confirmEmail', (req, res) => {
 app.post('/sendResetEmail', (req, res) => {
     
     const email = req.body.email;
-
     // look for an existing user with the email input from the send reset email page
     User.find({email: email}, (err, result) => {
-        
         if (err) {
             res.status(400).send(err);
         }
-
         else {
-
             // if user was found
             if (result && result.length > 0) {
-
                 // get user ID associated with incoming email address
                 const userID = result[0]._id;
-        
                 // send a password reset email with their userID as the URL parameter
                 sendPasswordResetEmail(email, userID);
-                
                 res.status(200).send('An email containing instructions on how to reset your password has been sent.');
             }
-
             else {
                 // if no user was found
                 res.status(400).send('No account was found linked to that email.');
@@ -221,28 +192,22 @@ app.post('/passwordChange', async(req, res) => {
     const userID = req.body.userid;
     // hash and salt new password with bcrypt
     const newPasswordHashed = await bcrypt.hash(req.body.newPassword, 10);
-
     // find user associated with userid, update password
     User.findOneAndUpdate({_id: userID}, {$set: {password: newPasswordHashed}}, (err, result) => {
-
         if (err) {
             console.log('An error occurred.');
             res.status(400).send(err);
         }
-    
         else {
-        
             // if user found
             if (result) {
                 res.status(200).send('Password successfully updated!');
             }
-
             // no user could be found with the given req userID
             else {
                 res.status(400).send('Invalid userID provided.');
             }
         }
-        
     });
 
 });
@@ -254,8 +219,10 @@ app.get('/getUser', (req, res) => {
 
 // log the user out (end the express session)
 app.get('/logout', (req, res) => {
+
     req.logout(req.user)
     res.status(200).send('You have successfully logged out!')
+
 })
 
 // make the server listen to a specific port (5000 in this case)
